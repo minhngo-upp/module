f02695fa13da13396b2b24b56fdc7e528ab23e47
minhngo-upp <minhngo-upp@users.noreply.github.com>
2026-05-05 11:35:00 +0700

    Fix Vercel base path

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import { ConfigProvider } from 'antd'
 import App from './App.jsx'
 import './index.css'
 
+const routerBaseName = import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/module">
+    <BrowserRouter basename={routerBaseName}>
       <ConfigProvider theme={{ token: { colorPrimary: '#22a06b', borderRadius: 8 } }}>
         <App />
       </ConfigProvider>
